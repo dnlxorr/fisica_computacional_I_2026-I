f4b18b90d98c77f4c49ac415f64b668be617dd36
sjaimescontreras-a11y <sjaimescontreras@gmail.com>
2026-03-13 13:38:54 -0500

    Medicion del tiempo Sharoon Jaimes

---
+++ SharoonJ/Tiempo_Rust_SharoonJ/src/main.rs
@@ -0,0 +1,19 @@
+use std::time::Instant;
+
+fn main() {
+
+    let inicio = Instant::now();
+
+    let mut suma = 0;
+
+    for i in 1..=1000 {
+        suma += i;
+        println!("Suma hasta {} = {}", i, suma);
+    }
+
+    let duracion = inicio.elapsed();
+
+    println!("--------------------------------");
+    println!("La suma total es: {}", suma);
+    println!("Tiempo de ejecución: {:?}", duracion);
+}
