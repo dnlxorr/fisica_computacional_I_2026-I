0da2c0c6dcd960799b4916264e932b87d8764d1d
CristianVarVer <cristianvargas212006@gmail.com>
2026-03-19 13:51:04 -0500

    Creacion del ejercicio de estructura de datos estructura_de_datos.rs

--- CristianV/CristianRust/Cargo/src/main.rs
+++ CristianV/CristianRust/Cargo/src/main.rs
@@ -1,14 +1,16 @@
 mod variable;
 mod pares_impares;
 mod tiempo_ejecucion;
-
-
+mod estructura_de_datos;
 
 fn main() {
-    print!("Ejecucion de tiempo \n");
+    /* print!("Ejecucion de tiempo \n");
     tiempo_ejecucion::tiempo_eje();
     print!("Ejecucion de variable\n ");
     variable::aja();
     print!("Ejecucion de Primos, Pares e Impares\n");
     pares_impares::num_pares_impares_primos();
+    */
+    print!("Ejecucion de Estructura de Datos\n");
+    estructura_de_datos::estructura();
 }
